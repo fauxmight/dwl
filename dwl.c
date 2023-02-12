591beebd16ae5f14452491f952474ee5c389dae7
A Frederick Christensen <dwl@ivories.org>
2023-02-12 09:56:05 -0600

    Accommodate tablet min,max

--- dwl.c
+++ dwl.c
@@ -2455,10 +2455,17 @@ tablettooltip(struct wl_listener *listener, void *data)
 void
 tablettoolaxis(struct wl_listener *listener, void *data)
 {
-	double mapped_x, mapped_y;
+	double	map_ratio_x = 0,
+		map_ratio_y = 0,
+		mapped_x = 0,
+		mapped_y = 0;
+	struct wlr_box mapping;
 	struct wlr_tablet_tool_axis_event *event = data;
-	mapped_x =  event->x * (tablet_max_x - tablet_min_x) + tablet_min_x;
-	mapped_y =  event->y * (tablet_max_y - tablet_min_y) + tablet_min_y;
+	wlr_output_layout_get_box(cursor->state->layout, NULL, &mapping);
+	map_ratio_x = (mapping.width  - (tablet_x_max - tablet_x_min)) / mapping.width;
+	map_ratio_y = (mapping.height - (tablet_y_max - tablet_y_min)) / mapping.height;
+	mapped_x = event->x * map_ratio_x + tablet_x_min;
+	mapped_y = event->y * map_ratio_y + tablet_y_min;
 	wlr_cursor_warp_absolute(cursor, &event->tablet->base,
 				 event->updated_axes & WLR_TABLET_TOOL_AXIS_X ? mapped_x : NAN,
 				 event->updated_axes & WLR_TABLET_TOOL_AXIS_Y ? mapped_y : NAN);
